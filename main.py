886c0486a778838007759bfda253d04be8e97f5e
jAlejandro <j.alejandrobetancourth23@gmail.com>
2024-10-22 22:30:11 -0500

    Más correcciones ortográficas

--- main.py
+++ main.py
@@ -104,13 +104,13 @@ def generate_template(subject, content):
         return (f"""
             <html>
             <head>
-                <title>Código de autenticacion</title>
+                <title>Código de autenticación</title>
             </head>
             <body>
                 <div style='font-family: Arial, sans-serif; background-color: #f6f6f6; margin: 0; padding: 20px;'> 
                     <div style='max-width: 600px; margin: auto; background: white; padding: 20px; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1);'> 
-                        <h1 style='color: #333;'>¡Código de autenticacion!</h1> 
-                        <p>Tu Código de autenticacion es el siguiente:</p> 
+                        <h1 style='color: #333;'>¡Código de autenticación!</h1> 
+                        <p>Tu Código de autenticación es el siguiente:</p> 
                         <div style='background-color: #e7f3fe; border: 1px solid #a6c8ff; padding: 10px; border-radius: 5px; font-family: monospace; font-size: 16px; color: #1a73e8;'>
                             {content}
                         </div> 
